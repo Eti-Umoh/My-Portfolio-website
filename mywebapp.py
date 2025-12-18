afbd7af68bc2adc4c09f8e4e08b924c133ecfc6f
Eti Umoh <etiumoh04@gmail.com>
2025-12-18 10:12:49 +0000

    Update contact form and email sending

--- mywebapp.py
+++ mywebapp.py
@@ -1,9 +1,8 @@
 import os
 from flask import Flask
-from flask import render_template,request,redirect,url_for
+from flask import render_template,redirect,url_for
 from forms import EmailForm
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from mailjet_rest import Client
 from dotenv import load_dotenv
 
 
@@ -12,7 +11,7 @@ app = Flask(__name__)
 ENV_PATH = '.env'
 load_dotenv(ENV_PATH)
 
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.secret_key = os.getenv("SECRET_KEY", "fallback-secret-key")
 
 @app.route('/')
 def index():
@@ -34,22 +33,38 @@ def contact_me():
         email = form.email.data
         messages = form.messages.data
 
-        SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-        SMTP_HOST_SENDER ='simeoneumoh@gmail.com'
+        MAILJET_API_KEY = os.getenv('MAILJET_API_KEY')
+        MAILJET_SECRET_KEY = os.getenv('MAILJET_SECRET_KEY')
+        SENDER_EMAIL = os.getenv('SENDER_EMAIL')
+        RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL')
+        subject="User Enquiry"
 
-        message = Mail(
-        from_email=SMTP_HOST_SENDER,
-        to_emails="etiumoh04@gmail.com",
-        subject="User Enquiry",
-        html_content=f"<p>This email is from {email}</p><p>{messages}</p>")
         try:
-            sg = SendGridAPIClient(SENDGRID_API_KEY)
-            response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
+            mailjet = Client(auth=(MAILJET_API_KEY, MAILJET_SECRET_KEY), version='v3.1')
+            data = {
+            'Messages': [
+                            {
+                                "From": {
+                                        "Email": SENDER_EMAIL,
+                                        "Name": "Portfolio website"
+                                },
+                                "To": [
+                                        {
+                                                "Email": RECEIVER_EMAIL,
+                                                "Name": "Receiver Email"
+                                        }
+                                ],
+                                "Subject": subject,
+                                "TextPart": f"This email is from {email}. {messages}",
+                                "HTMLPart": f"""<h1>This email is from {email}</h1><br/><h3>{messages}</h3>"""
+                            }
+                    ]
+            }
+            result = mailjet.send.create(data=data)
+            print(result.status_code)
+            print(result.json())
         except Exception as e:
-            print(e.message)
+            print(str(e))
         return redirect(url_for('contact_me'))
     return render_template('contact_me.html',form=form)
 
